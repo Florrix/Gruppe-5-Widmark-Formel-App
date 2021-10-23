c29e52a38f5d4237a5d1aec4dba70666f71e7dc9
Peter von Hahn <pe.vo.ha@web.de>
2021-10-23 15:45:53 +0200

    New Page added
    Startpage: Button hinzugefügt
    resultPage: erstellt

---
+++ app/src/main/java/com/mobileapplications/gruppe_5_widmark_formel_app/ResultFragment.kt
@@ -0,0 +1,22 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.mobileapplications.gruppe_5_widmark_formel_app.R
+import com.mobileapplications.gruppe_5_widmark_formel_app.databinding.FragmentStartBinding
+
+class ResultFragment : Fragment() {
+    private lateinit var binding: FragmentStartBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_result, container, false)
+
+        return binding.root
+    }
+}
