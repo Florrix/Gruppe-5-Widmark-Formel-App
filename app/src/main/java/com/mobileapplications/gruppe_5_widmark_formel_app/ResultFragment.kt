2a9ae898189b61a6ffab9deb2a2d26b7b8f7eb7e
YessicaIlgner <90403765+YessicaIlgner@users.noreply.github.com>
2021-10-23 16:20:01 +0200

    Update misc.xml

--- app/src/main/java/com/mobileapplications/gruppe_5_widmark_formel_app/ResultFragment.kt
+++ app/src/main/java/com/mobileapplications/gruppe_5_widmark_formel_app/ResultFragment.kt
@@ -1,28 +1,25 @@
 package com.mobileapplications.gruppe_5_widmark_formel_app
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainer
-import androidx.navigation.findNavController
 import com.mobileapplications.gruppe_5_widmark_formel_app.R
 import com.mobileapplications.gruppe_5_widmark_formel_app.databinding.FragmentStartBinding
 
-class StartFragment : Fragment() {
+class ResultFragment : Fragment() {
     private lateinit var binding: FragmentStartBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_start, container, false)
-        binding.buttonCalculate.setOnClickListener{
-            view: View -> view.findNavController().navigate(R.id.toResultFragment)
-        }
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_result, container, false)
 
         return binding.root
     }
-
 }
