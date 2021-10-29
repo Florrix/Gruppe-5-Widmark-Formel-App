fedd82817029370caa4daa21694236957e05b49d
Peter von Hahn <pe.vo.ha@web.de>
2021-10-29 15:09:18 +0200

    Fixed Database problems

--- app/src/main/java/com/mobileapplications/gruppe_5_widmark_formel_app/database/ResultDao.kt
+++ app/src/main/java/com/mobileapplications/gruppe_5_widmark_formel_app/database/ResultDao.kt
@@ -1,10 +1,12 @@
 package com.mobileapplications.gruppe_5_widmark_formel_app.database
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
+@Dao
 interface ResultDao {
     @Query("SELECT * FROM result_table ORDER BY id ASC")
     fun getAllResultsSortedById(): LiveData<List<Result>>
--- app/src/main/java/com/mobileapplications/gruppe_5_widmark_formel_app/database/ResultDatabase.kt
+++ app/src/main/java/com/mobileapplications/gruppe_5_widmark_formel_app/database/ResultDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [ContactsContract.CommonDataKinds.Note::class], version = 1, exportSchema = false)
+@Database(entities = [Result::class], version = 1, exportSchema = false)
 abstract class ResultDatabase: RoomDatabase() {
     abstract val resultDao: ResultDao
 
