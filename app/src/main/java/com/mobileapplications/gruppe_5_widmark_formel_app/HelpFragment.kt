6ba6b98888f83922c4e895dc8c48326db908dac9
Peter von Hahn <pe.vo.ha@web.de>
2021-10-29 14:01:23 +0200

    Revert "Layoutchange alcoholinput"
    This reverts commit c0d2ce87b26c6cc832b651c204dc6196e6acdb10.

--- app/src/main/java/com/mobileapplications/gruppe_5_widmark_formel_app/HelpFragment.kt
+++ app/src/main/java/com/mobileapplications/gruppe_5_widmark_formel_app/HelpFragment.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.mobileapplications.gruppe_5_widmark_formel_app.R
 import com.mobileapplications.gruppe_5_widmark_formel_app.databinding.FragmentHelpBinding
+import com.mobileapplications.gruppe_5_widmark_formel_app.databinding.FragmentStartBinding
 
 class HelpFragment : Fragment() {
     private lateinit var binding: FragmentHelpBinding
